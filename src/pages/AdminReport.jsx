8b84c93a894db4051a541176267581a9b71c05eb
semahegn-tilahun <yegnabetsew@gmail.com>
2026-02-18 05:26:59 +0300

    importing useNavigate react-router-dom component

--- src/pages/AdminReport.jsx
+++ src/pages/AdminReport.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
 import AdminLayout from "../components/AdminLayout";
-
+import { useNavigate } from "react-router-dom";
 export default function AdminReport() {
   const [report, setReport] = useState([]);
   const navigate = useNavigate();
