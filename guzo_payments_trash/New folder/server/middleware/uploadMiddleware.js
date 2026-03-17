0b9a830743d7a0e1f8831969593b8f1d32c3836f
semahegn-tilahun <yegnabetsew@gmail.com>
2026-03-17 22:12:58 +0300

    Unnecessary docs

--- guzo_payments_trash/New folder/server/middleware/uploadMiddleware.js
+++
@@ -1,8 +0,0 @@
-import multer from "multer";
-
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
-});
-
-export default multer({ storage });
