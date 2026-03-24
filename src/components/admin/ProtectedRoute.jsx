7f19f1609d67633662e9dba2c88bfc0c0d908a95
semahegn-tilahun <yegnabetsew@gmail.com>
2026-03-24 22:08:28 +0300

    Update UI ooter

--- src/components/admin/ProtectedRoute.jsx
+++ src/components/admin/ProtectedRoute.jsx
@@ -1,10 +1,3 @@
-// import { Navigate } from "react-router-dom";
-
-// export default function ProtectedRoute({ children }) {
-//   const token = localStorage.getItem("adminToken");
-//   if (!token) return <Navigate to="/admin-login" replace />;
-//   return children;
-// }
 import { Navigate, useLocation } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,3 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "https://server-y72m.onrender.com/api",
-// });
-
-// api.interceptors.request.use((config) => {
-//   // ✅ Use adminToken first, fallback to token
-//   const token =
-//     localStorage.getItem("adminToken") || localStorage.getItem("token");
-
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// export default api;
 import axios from "axios";
 
 const api = axios.create({
